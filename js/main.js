cebe1b653bcf15425dfb0aef998f094138781dc2
Yuzhnii <70344381+108etep@users.noreply.github.com>
2021-12-07 19:39:06 +0500

    next fixxed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-$(function (){
-   
-})
+// Это попап\всплывашка
+const closeBtn = document.querySelector('.biscard__socnet-close');
+const tooltip = document.querySelector('.header__quest-inner');
+
+setTimeout(popUpVisible, 3000);
+
+closeBtn.addEventListener('click',(e) => {
+  e.preventDefault()
+  tooltip.style.transform = "translateX(-200vh)";
+  
+});
+
+function popUpVisible() {
+tooltip.style.transform = "translateX(0vh)"
+};
+// конец всплывашки
